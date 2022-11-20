07630fbd054a04da5841ea390693f9a7b0387956
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2022-11-20 07:33:47 +0300

    add `build_dist.ts` for quick minified bundling, via `esbuild`

---
+++ build_dist.ts
@@ -0,0 +1,19 @@
+import { build as esbuild, stop as esstop } from "https://deno.land/x/esbuild/mod.js"
+import { denoPlugin } from "https://deno.land/x/esbuild_deno_loader/mod.ts"
+
+let t0 = performance.now(), t1: number
+await esbuild({
+	entryPoints: ["./mod.ts"],
+	outdir: "./dist/",
+	bundle: true,
+	minify: true,
+	platform: "neutral",
+	format: "esm",
+	target: "esnext",
+	plugins: [denoPlugin()],
+	define: {},
+})
+esstop()
+t1 = performance.now()
+console.log("execution time:", t1 - t0, "ms")
+console.log("dist binary size:", Deno.statSync("./dist/mod.js").size / 1024, "kb")
