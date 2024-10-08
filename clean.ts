1b25f1ecf951396fe7ab0f8e678fde03dd7743ca
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2024-10-08 08:22:10 -0400

    update repo tooling: - purge `/build_dist.ts` in favor of `jsr:@oazmi/build-tools@0.2.1/cli/dist` deno task. - purge `/build_docs.ts` in favor of `jsr:@oazmi/build-tools@0.2.1/cli/docs`. - purge `/build_npm.ts` in favor of `jsr:@oazmi/build-tools@0.2.1/cli/npm`. - purge `/build_tools.ts` since `jsr:@oazmi/build-tools` takes care of the build processes. - modernize `/clean.ts`. - in `/deno.json`, add fields `buildDist`, `buildDocs`, and `buildNpm` for configuring the build tasks of `jsr:@oazmi/build-tools`.

--- clean.ts
+++ clean.ts
@@ -1,30 +1,53 @@
-/** clean the project directory */
+/** clean this project's temporary directories and files. */
+
+const
+	shell_args = new Set(Deno.args),
+	dryrun = shell_args.delete("--dryrun"),
+	dirs_only = shell_args.delete("--dirs-only"),
+	files_only = shell_args.delete("--files-only"),
+	shell_rest_args = [...shell_args]
 
 const delete_dir_list: string[] = [
-	// "./backup",
 	"./docs/",
 	"./dist/",
 	"./npm/",
 	"./node_modules/",
 	"./temp/",
+	/*
+	"./backup/",
+	*/
 ]
 
 const delete_file_list: string[] = [
 	"./deno.d.ts",
-	// "./deno.lock",
 	"./package.json",
 	"./tsconfig.json",
 	"./typedoc.json",
+	/*
+	"deno.lock",
+	*/
 ]
 
-let stat: Deno.FileInfo
-for (const dir_path of delete_dir_list) {
-	try { stat = Deno.statSync(dir_path) }
-	catch (error) { continue }
-	if (stat.isDirectory) Deno.removeSync(dir_path, { recursive: true })
+if (dirs_only || !files_only) {
+	await Promise.all(delete_dir_list.map(async (dir_path) => {
+		try {
+			const stat = await Deno.stat(dir_path)
+			if (stat.isDirectory) {
+				console.log("[in-fs] deleting directory:", dir_path)
+				if (!dryrun) { await Deno.remove(dir_path, { recursive: true }) }
+			}
+		} catch (error) { console.warn("directory does not exist:", dir_path) }
+	}))
 }
-for (const file_path of delete_file_list) {
-	try { stat = Deno.statSync(file_path) }
-	catch (error) { continue }
-	if (stat.isFile) Deno.removeSync(file_path, { recursive: true })
+
+if (files_only || !dirs_only) {
+	await Promise.all(delete_file_list.map(async (file_path) => {
+		try {
+			const stat = await Deno.stat(file_path)
+			if (stat.isFile) {
+				console.log("[in-fs] deleting file:", file_path)
+				if (!dryrun) { await Deno.remove(file_path) }
+			}
+		} catch (error) { console.warn("file does not exist:", file_path) }
+	}))
 }
