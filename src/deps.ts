d120c14af8006bbc5c47394f249d1eac556efa3b
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2024-10-18 02:21:25 -0400

    - bump patch version to `0.8.2`. - export the `timeman` submodule in `/src/timeman.ts` in `/src/mod.ts` and `/deno.json`. - breaking changes:   - rename `/src/crypto.ts` to `/src/cryptoman.ts`.   - rename `/src/path.ts` to `/src/pathman.ts`.

--- src/deps.ts
+++ src/deps.ts
@@ -11,10 +11,10 @@ export const enum DEBUG {
 
 // TODO: before releasing version `0.9.0`, take care of the following:
 // - [ ] rename the following submodules (and also take care of potential doc link breakdown when using `!module_name`):
-//   - [ ] `builtin_aliases` -> `builtin`
-//   - [ ] `builtin_aliases_deps` -> `builtin_deps`
-//   - [ ] `crypto` -> `cryptoman` ? unsure about this one
-//   - [ ] `path` -> `pathman`
+//   - [ ] `builtin_aliases` -> `aliases`
+//   - [ ] `builtin_aliases_deps` -> `aliases_deps`
+//   - [x] `crypto` -> `cryptoman` ? unsure about this one
+//   - [x] `path` -> `pathman`
 // - [ ] fix the doc examples so that they do not error when `deno test --doc "./src/"` is ran.
 // - [ ] populate the doc examples with legitimate test cases using `"jsr:@std/assert"` for assertion.
 // - [ ] fix the `slice` and `splice` (and maybe there are other methods as well) method issue with your custom subclasses of `Array`, `Map`, and `Set`,
@@ -23,3 +23,7 @@ export const enum DEBUG {
 // - [ ] add your "AWS Signature Version 4" computation algorithm to `cryptoman.ts`.
 //       for that, you will also have to create aliases for `Crypto.subtle`.
 //       but the tradeoff it has is that `Crypto.subtle` is only available in "https" (secure) website contexts, and standalone js runtimes (deno, cloudflare workers, node, bun, etc...)
+// - [ ] add `promiseman` submodule that specializes in promise based utility functions.
+// - [ ] migrate the following functions and objects to the `timeman`, `aliases_deps`, and `promiseman` submodules, as you see fit:
+//   - `promiseTimeout`, `debounce`, `debounceAndShare`, `throttle`, `throttleAndTrail`, `THROTTLE_REJECT`, `TIMEOUT`
+// - [ ] migrate data uri manipilation/parsing features in the `image` submodule to the `pathman` submodule.
