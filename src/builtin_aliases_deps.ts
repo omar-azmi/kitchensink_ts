59b13946b30e1ae56657dc3c3d7883c0c3b23ad5
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2024-10-23 12:04:57 -0400

    in `/src/pathman.ts` make the following additions and changes: - add `"blob"` as a detectable `UriScheme`. - replace all instances of `"./"` and `"../"` directory navigators with the constant variables `dotslash` and `dotdotslash`. - replace all `string` calls to `startsWith` and `endsWith` methods with the minifiable `string_starts_with` and `string_ends_with` methods. - add the function `isAbsolutePath` which identifies if a given path is absolute or relative (compatible with common posix and windows path patterns). - in `getUriScheme`, we now use the `isAbsolutePath` function to distinguish between absolute (`"local"`) and relative (`"relative"`) paths.   this also introduces a breaking change:     previously, paths beginning with any character that was not either `"./"` or `"../"` were identified as absolute, but now, they are usually identified as relative paths (except for windows-like absolute paths such as `"C:/"` etc...).     this was an intentional change, as posix and windows style paths that begin with any valid file/folder-name character are considered to be relative to the current working directory, but my old `getUriScheme` did not take that into consideration. - in the `resolveAsUrl` function, ensure that we apply `encodeURI` to the path before constructing `URL` objects.   this change was not necessary, as the `URL` constructor does internally re-encode the input path to ensure that there are no illegal path characters (such as blank space). but it is good to make sure that we also take care of it on our own. - add `resolvePosixPathFactory` and `resolvePathFactory` functions, which provide a factory function for generating a path-resolver function that is interoperable with python's `pathlib`'s `Path.resolve` method, and also deno std's `path.resolve` function.   the reason for creating a factory function rather than a path resolving function is because a path-resolve function requires some context about where it is being executed from (i.e. what is the absolute path), and embedding such information will create a dependency on `Deno`, making it incompatible with browsers.   thus a much better approach is to let the user define their own dynamic or static working path (it can even be an http url), and their own optional set of rules that declare what is absolute and what is not, to allow a user from any context to use a convenient path-resolution function. - port over my beta `globPatternToRegex` function from `build_tools_ts`: omar-azmi/build_tools_ts@b73d54ba952803c2ea7d72018c4a6465119cf3d8   I may improve it in the future, or deprecate it. its future is uncertain, and so is yours. shine' kudasai, onegai, arigatai.

--- src/builtin_aliases_deps.ts
+++ src/builtin_aliases_deps.ts
@@ -94,7 +94,8 @@ export const
 	dom_setTimeout = setTimeout,
 	dom_clearTimeout = clearTimeout,
 	dom_setInterval = setInterval,
-	dom_clearInterval = clearInterval
+	dom_clearInterval = clearInterval,
+	dom_encodeURI = encodeURI
 
 export const {
 	assert: console_assert,
