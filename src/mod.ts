97fb43ea0da721ea189a5daafd526c77662a9020
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2024-11-17 12:10:56 -0500

    - bump patch version to `0.8.5`. - [breaking change]: remove `binder.ts` exportation from the default library-module `/src/mod.ts`.

--- src/mod.ts
+++ src/mod.ts
@@ -1,12 +1,18 @@
-/** auto export for all modules, except {@link "devdebug"}. <br>
- * `devdebug` pollutes the `globalThis` object whenever imported.
- * thus, anyone desiring this module should import it using `import {...} from "@oazmi/kitchensink/devdebug"`. <br>
- * also, {@link "alias"} is not exported either to avoid namespace pollution in IDEs <br>
+/** auto export for all submodules, with the exception of the following:
+ * - {@link "alias"}:
+ *   it will pollute the global namespace, making it harder for IDEs to give useful suggestions.
+ *   in addition, the bundled `kitchensink` library will be inflated with these un-treeshaken exports.
+ * - {@link "binder"}:
+ *   this submodule will add a lot of un-treeshaken baggage, and it will probably not be used by the end user.
+ * - {@link "devdebug"}:
+ *   this submodule pollutes the `globalThis` object whenever imported, so it is not exported here.
+ * 
+ * anyone desiring to import one of these submodules should import them directly using `import {...} from "@oazmi/kitchensink/submodule_name"`.
 */
 
 // export * from "./alias.ts"
 export * from "./array2d.ts"
-export * from "./binder.ts"
+// export * from "./binder.ts"
 export * from "./browser.ts"
 export * from "./collections.ts"
 export * from "./cryptoman.ts"
