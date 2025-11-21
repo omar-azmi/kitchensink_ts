653d9496583171589a5210c930462e64f9db4aa5
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2025-11-21 16:55:43 -0500

    - in `/src/network/tcp.ts`, add an implementation of `NetConn` over txiki.js's tcp-connection (`tjs.Connection`). - in `/src/network/udp.ts`, add an implementation of `NetConn` over txiki.js's udp-connection (`tjs.DatagramEndpoint`).

--- src/network/tcp.ts
+++ src/network/tcp.ts
@@ -1,15 +1,19 @@
-import { type NetAddr, SIZE, type NetConn, type NetConnReadValue } from "./conn.ts"
+import { SIZE, type NetAddr, type NetConn, type NetConnReadValue } from "./conn.ts"
 
 
+/** a {@link NetConn} interface implementation wrapper for `Deno.connect` (deno's tcp implementation). */
 export class DenoTcpNetConn implements NetConn {
 	protected readonly base: Deno.Conn<Deno.NetAddr>
 	protected readonly buf: Uint8Array
+	protected readonly remoteAddr: NetAddr
 	public readonly size: number
 
 	constructor(conn: Deno.Conn, bring_your_own_buffer?: Uint8Array) {
 		this.base = conn as any
 		this.buf = bring_your_own_buffer ?? new Uint8Array(SIZE.BufferBytes)
 		this.size = this.buf.byteLength
+		const { hostname, port } = (conn as Deno.Conn<Deno.NetAddr>).remoteAddr
+		this.remoteAddr = { hostname, port, family: 4 }
 	}
 
 	async read(): Promise<NetConnReadValue> {
@@ -21,9 +25,8 @@ export class DenoTcpNetConn implements NetConn {
 		const
 			base = this.base,
 			buf = this.buf,
-			{ hostname, port } = base.remoteAddr,
 			bytes_read = await base.read(this.buf) ?? 0
-		return [buf.slice(0, bytes_read), { hostname, port, family: 4 }]
+		return [buf.slice(0, bytes_read), { ...this.remoteAddr }]
 	}
 
 	async send(buffer: Uint8Array, addr?: NetAddr): Promise<number> {
@@ -32,6 +35,48 @@ export class DenoTcpNetConn implements NetConn {
 			base = this.base,
 			bytes_to_write = buffer.byteLength
 		// yes, we should permit empty messages, even if the underlying `base` class ignores zero sized messages.
+		if (bytes_to_write === 0) { return base.write(buffer) }
+		while (bytes_written < bytes_to_write) {
+			bytes_written += await base.write(buffer.subarray(bytes_written))
+		}
+		// TODO: should we assert `bytes_written === bytes_to_write`?
+		return bytes_written
+	}
+
+	close(): void {
+		this.base.close()
+	}
+}
+
+/** a {@link NetConn} interface implementation wrapper for `tjs.connect("tcp", ...)` (txiki.js's tcp implementation). */
+export class TjsTcpNetConn implements NetConn {
+	protected readonly base: tjs.Connection
+	protected readonly buf: Uint8Array
+	protected readonly remoteAddr: NetAddr
+	public readonly size: number
+
+	constructor(conn: tjs.Connection, bring_your_own_buffer?: Uint8Array) {
+		this.base = conn
+		this.buf = bring_your_own_buffer ?? new Uint8Array(SIZE.BufferBytes)
+		this.size = this.buf.byteLength
+		const { ip: hostname, port, family } = conn.remoteAddress
+		this.remoteAddr = { hostname, port, family: family as any }
+	}
+
+	async read(): Promise<NetConnReadValue> {
+		const
+			base = this.base,
+			buf = this.buf,
+			bytes_read = await base.read(this.buf) ?? 0
+		return [buf.slice(0, bytes_read), { ...this.remoteAddr }]
+	}
+
+	async send(buffer: Uint8Array, addr?: NetAddr): Promise<number> {
+		let bytes_written = 0
+		const
+			base = this.base,
+			bytes_to_write = buffer.byteLength
+		// yes, we should permit empty messages, even if the underlying `base` class may ignore zero sized messages.
 		if (bytes_to_write === 0) { return base.write(buffer) }
 		while (bytes_written < bytes_to_write) {
 			bytes_written += await base.write(buffer.subarray(bytes_written))
--- src/network/udp.ts
+++ src/network/udp.ts
@@ -1,7 +1,8 @@
 import { number_MAX_SAFE_INTEGER } from "../alias.ts"
-import type { NetAddr, NetConn, NetConnReadValue } from "./conn.ts"
+import { SIZE, type NetAddr, type NetConn, type NetConnReadValue } from "./conn.ts"
 
 
+/** a {@link NetConn} interface implementation wrapper for `Deno.listenDatagram` (deno's udp implementation). */
 export class DenoUdpNetConn implements NetConn {
 	protected readonly base: Deno.DatagramConn
 	public readonly size: number
@@ -31,6 +32,49 @@ export class DenoUdpNetConn implements NetConn {
 		if (bytes_to_write === 0) { return base.send(buffer, deno_addr) }
 		while (bytes_written < bytes_to_write) {
 			bytes_written += await base.send(buffer.subarray(bytes_written), deno_addr)
+		}
+		// TODO: should we assert `bytes_written === bytes_to_write`?
+		return bytes_written
+	}
+
+	close(): void {
+		this.base.close()
+	}
+}
+
+/** a {@link NetConn} interface implementation wrapper for `tjs.connect("udp", ...)` (txiki.js's udp implementation). */
+export class TjsUdpNetConn implements NetConn {
+	protected readonly base: tjs.DatagramEndpoint
+	protected readonly buf: Uint8Array
+	public readonly size: number
+
+	constructor(conn: tjs.DatagramEndpoint, bring_your_own_buffer?: Uint8Array) {
+		this.base = conn
+		this.buf = bring_your_own_buffer ?? new Uint8Array(SIZE.DatagramMtu)
+		this.size = this.buf.byteLength
+	}
+
+	async read(): Promise<NetConnReadValue> {
+		const
+			base = this.base,
+			buf = this.buf,
+			{ addr: tjs_addr, nread, partial } = await base.recv(buf) as unknown as tjs.DatagramData, // the return type on tjs is incorrect.
+			{ ip: hostname, port, family } = tjs_addr
+		return [buf.slice(0, nread), { hostname, port, family: family as any }]
+	}
+
+	async send(buffer: Uint8Array, addr: NetAddr): Promise<number> {
+		let bytes_written = 0
+		const
+			base = this.base,
+			bytes_to_write = buffer.byteLength,
+			{ hostname: ip, port, family } = addr,
+			tjs_addr: tjs.Address = { ip, port, family }
+		// yes, we should permit empty messages, even if the underlying `base` class may ignore zero sized messages.
+		if (bytes_to_write === 0) { return base.send(buffer, tjs_addr) as unknown as number } // the return type on tjs is incorrect.
+		while (bytes_written < bytes_to_write) {
+			// the return type on tjs is incorrect.
+			bytes_written += await base.send(buffer.subarray(bytes_written), tjs_addr) as unknown as number
 		}
 		// TODO: should we assert `bytes_written === bytes_to_write`?
 		return bytes_written
